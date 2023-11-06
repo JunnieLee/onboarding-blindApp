3ecdcd37de852ff0ab9ec80e6b0c3c32a58c81af
JunnieLee <heunghari@naver.com>
2023-11-06 15:05:23 +0900

    feat: 서버쪽 정보를 담기 위한 ContentDto 생성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,5 +66,10 @@ dependencies {
     annotationProcessor("androidx.room:room-compiler:$roomVersion")
     kapt("androidx.room:room-compiler:$roomVersion")
 
+    //Retrofit
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
 
 }
