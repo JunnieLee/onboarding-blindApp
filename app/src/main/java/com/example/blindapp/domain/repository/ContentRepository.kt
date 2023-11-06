afe2632574c5a3c2d52f45d3d3bafd18a28df609
JunnieLee <heunghari@naver.com>
2023-11-06 17:06:36 +0900

    feat: Content Repository 생성

---
+++ app/src/main/java/com/example/blindapp/domain/repository/ContentRepository.kt
@@ -0,0 +1,15 @@
+package com.example.blindapp.domain.repository
+
+import com.example.blindapp.domain.model.Content
+import kotlinx.coroutines.flow.Flow
+
+interface ContentRepository {
+
+    fun loadList(): Flow<List<Content>>
+
+    suspend fun insert(item: Content): Boolean
+
+    suspend fun update(item: Content): Boolean
+
+    suspend fun delete(item: Content): Boolean
+}
