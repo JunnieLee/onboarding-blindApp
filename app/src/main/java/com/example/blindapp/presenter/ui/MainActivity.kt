5b1d6174467d1f228736e9efc6e70b9875ae94b9
JunnieLee <heunghari@naver.com>
2023-11-06 18:18:52 +0900

    feat: mainViewModel 추가 후 MainActivity 코드와 연동

--- app/src/main/java/com/example/blindapp/presenter/ui/MainActivity.kt
+++ app/src/main/java/com/example/blindapp/presenter/ui/MainActivity.kt
@@ -3,16 +3,27 @@ package com.example.blindapp.presenter.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
+import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.blindapp.R
 import com.example.blindapp.databinding.ActivityMainBinding
 import com.example.blindapp.domain.model.Content
 import com.example.blindapp.presenter.ui.list.ListAdapter
+import com.example.blindapp.presenter.viewModel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val viewModel: MainViewModel by viewModels()
 
     private val adapter by lazy { ListAdapter(Handler()) }
 
@@ -29,9 +40,27 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         }
+        observeViewModel()
     }
 
     fun onClickAdd(){
+        InputActivity.start(this)
+    }
+
+    private fun observeViewModel() {
+        lifecycleScope.launch {
+            viewModel.contentList
+                .flowWithLifecycle(lifecycle, Lifecycle.State.RESUMED)
+                .collect {
+                    binding.apply {
+                        progressBar.isVisible = false
+                        emptyTextView.isVisible = it.isEmpty()
+                        recyclerView.isVisible = it.isNotEmpty()
+                    }
+                    adapter.submitList(it)
+                }
+        }
+
 
     }
 
