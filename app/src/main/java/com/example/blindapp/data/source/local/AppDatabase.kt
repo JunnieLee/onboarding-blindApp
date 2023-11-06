8bccf30c0927f106aee10adacf48241fc5452b59
JunnieLee <heunghari@naver.com>
2023-11-06 14:39:06 +0900

    feat: 앞서 생성한 Content Entity와 Type Converter 이용해서 AppDatabase 생성

---
+++ app/src/main/java/com/example/blindapp/data/source/local/AppDatabase.kt
@@ -0,0 +1,13 @@
+package com.example.blindapp.data.source.local
+
+import androidx.room.Database
+import androidx.room.TypeConverters
+import com.example.blindapp.data.model.entity.ContentEntity
+import com.example.blindapp.data.source.local.dao.ContentDao
+
+@Database(entities=[ContentEntity::class], version=1)
+@TypeConverters(DateConverter::class)
+abstract class AppDatabase {
+
+    abstract fun contentDao(): ContentDao
+}
